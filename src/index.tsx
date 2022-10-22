c986a847e6d0e4df6d1ef7d6ea34a578a97efd79
Nickithine <dominique.ithina@gmail.com>
2022-10-22 19:29:06 -0300

    feat: Distribuindo os dados da requisição
    Preenchendo os dados da lista de transações com os dados do get da requisição.

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,34 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-10-22 19:09:58'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1200,
+          createdAt: new Date('2022-10-14 11:09:58'),
+        },
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions');
+      return this.schema.all('transaction');
     });
 
     this.post('/transactions', (schema, request) => {
