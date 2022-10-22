c48366ea575b8c87d3284e8ea4981ae4249b2c89
Nickithine <dominique.ithina@gmail.com>
2022-10-22 18:15:48 -0300

    feat: Criando novo componente de modal
    Estilizando novo modal, seus inputs e botoes, e adicionando novos pacotes para melhor
    usabilidade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
   return(
     <Container>
       <Content>
         <img src={ logoImg } alt="domi money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal} >
           Nova transação
         </button>
+        
       </Content>
     </Container>
   )
